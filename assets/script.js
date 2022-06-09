1d35cb0f061b2c0ef170f360bde4d0ad942c109c
livmeierx <liv.meier13@gmail.com>
2022-06-08 20:17:29 -0400

    rearrange js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,48 @@
 var saveBtn = $(".saveBtn");
 
-$("#currentDay").text(dayjs().format('dddd MMMM Do YYYY'));
+$(document).ready(function(){
+    $(".saveBtn").on("click", function(){
+        var value = $(this).siblings(".plan").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+    });
 
-function timeBlock() {
-    var hour = dayjs().hours();
+    function updateHour() {
+        var hour = moment().hours();
+    
+        $(".time-block").each(function() {
+            var timeNow = parseInt($(this).attr("id").split("-")[1]);
+    
+            if (timeNow < hour) {
+                $(this).addClass("past");
+            } 
+            else if (timeNow === hour) {
+                $(this).removClass("past");
+                $(this).addClass("present");
+            } 
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    };
+    
+updateHour();
 
-    $(".time-block").each(function() {
-        var timeNow = parseInt($(this).attr("id"));
+var interval = setInterval(hourUpdater, 15000);
+$("hour8 .plan").val(localStorage.getItem("hour8"));
+$("hour9 .plan").val(localStorage.getItem("hour9"));
+$("hour10 .plan").val(localStorage.getItem("hour10"));
+$("hour11 .plan").val(localStorage.getItem("hour11"));
+$("hour12 .plan").val(localStorage.getItem("hour12"));
+$("hour13 .plan").val(localStorage.getItem("hour13"));
+$("hour14 .plan").val(localStorage.getItem("hour14"));
+$("hour15 .plan").val(localStorage.getItem("hour15"));
+$("hour16 .plan").val(localStorage.getItem("hour16"));
+$("hour17 .plan").val(localStorage.getItem("hour17"));
 
-        if (timeNow > hour) {
-            $(this).addClass("future");
-        } 
-        else if (timeNow === hour) {
-            $(this).addClass("present");
-        } 
-        else {
-            $(this).addClass("past");
-        }
-    })
-};
+$("#currentDay").text(moment().format('dddd, MMMM, Do'));
+
+});
 
